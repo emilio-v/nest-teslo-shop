0c2977ba81797395a34f60764ac7afdf326e6cb7
Emilio Velasco <emilio_785@hotmail.com>
2024-05-15 23:53:15 -0600

    feat: create product DTO

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  sizes: string[];
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString()
+  @MinLength(1)
+  title: string;
+}
