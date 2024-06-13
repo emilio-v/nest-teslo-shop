77b7a518c61d913eda9ce79b30f2ab70e6a1c1c9
Emilio Velasco <62970075+emilio-v@users.noreply.github.com>
2024-06-13 00:26:41 -0600

    feat: find a product by UUID or slug (#4)

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
@@ -42,11 +43,19 @@ export class ProductsService {
     }
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
+    // const product = await this.productRepository.findOneBy({ id });
 
     if (!product)
-      throw new NotFoundException(`Product with id: ${id} not found`);
+      throw new NotFoundException(`Product with term: ${term} not found`);
 
     return product;
   }
