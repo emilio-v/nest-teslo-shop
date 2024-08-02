e8d139d32101e2c861a5d7f45e68fc4f9cd22046
Emilio Velasco <emilio_785@hotmail.com>
2024-08-01 21:03:52 -0600

    feat: return secure url

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -13,10 +13,14 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
 import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get('product/:imageName')
   findProductImage(
@@ -43,8 +47,10 @@ export class FilesController {
       throw new BadRequestException('Make sure that the file is an image.');
     }
 
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
     return {
-      file: file.filename,
+      secureUrl,
     };
   }
 }
