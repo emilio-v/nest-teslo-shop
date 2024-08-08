d2c212f34a23a6617c2c7f7588dc6bca19a410f1
Emilio Velasco <emilio_785@hotmail.com>
2024-08-08 00:15:34 -0600

    feat: create user

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -14,9 +14,11 @@ export class User {
   password: string;
 
   @Column('text')
-  fullname: string;
+  fullName: string;
 
-  @Column('bool')
+  @Column('bool', {
+    default: true,
+  })
   isActive: boolean;
 
   @Column('text', { array: true, default: ['user'] })
