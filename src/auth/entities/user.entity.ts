b905152cd495d518eac2f5a378c6b699583cf67b
Emilio Velasco <emilio_785@hotmail.com>
2024-09-15 00:17:03 -0600

    feat: create one-to-many relation between user and product

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,8 +1,10 @@
+import { Product } from 'src/products/entities';
 import {
   BeforeInsert,
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 
@@ -31,6 +33,9 @@ export class User {
 
   @Column('text', { array: true, default: ['user'] })
   roles: string[];
+
+  @OneToMany(() => Product, (product) => product.user)
+  product: Product;
 
   @BeforeInsert()
   checkEmailBeforeInsert() {
