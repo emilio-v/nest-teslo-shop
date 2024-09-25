eb5ac4e10cc6aabce72cec671586953039d8f3e4
Emilio Velasco <emilio_785@hotmail.com>
2024-09-25 14:47:13 -0600

    OpenAPI - swagger documentation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -10,40 +11,73 @@ import {
 } from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+  })
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty({
+    description: 'Product gender',
+    nullable: false,
+  })
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty({
+    description: 'Product price',
+  })
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty({
+    description: 'Product sizes',
+    nullable: false,
+  })
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty({
+    description: 'Product slug',
+  })
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty({
+    description: 'Product stock',
+    minimum: 0,
+  })
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty({
+    description: 'Product title',
+    nullable: false,
+    uniqueItems: true,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty({
+    description: 'Product tags',
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags: string[];
 
+  @ApiProperty({
+    description: 'Product images',
+  })
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -9,6 +9,7 @@ import {
   ParseUUIDPipe,
   Query,
 } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -16,13 +17,20 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { ValidRoles } from 'src/auth/interfaces';
 import { User } from 'src/auth/entities/user.entity';
+import { Product } from 'src/products/entities';
 
+@ApiTags('Products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
   @Auth(ValidRoles.admin)
+  @ApiResponse({
+    status: 201,
+    description: 'Product was created',
+    type: Product,
+  })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(user, createProductDto);
   }
