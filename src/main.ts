045123de7db22212aea6a873337ea774800658f6
Emilio Velasco <62970075+emilio-v@users.noreply.github.com>
2024-09-25 14:47:41 -0600

    OpenAPI - swagger documentation (#14)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('Teslo Shop endpoints')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api');
 
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -7,53 +7,98 @@ import {
   OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 import { ProductImage } from './product-image.entity';
 import { User } from 'src/auth/entities/user.entity';
 
 @Entity({ name: 'products' })
 export class Product {
+  @ApiProperty({
+    example: 'abcb6194-0031-4e1f-a330-81a179ca68ab',
+    description: 'Product Id',
+    uniqueItems: true,
+  })
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty({
+    example: 'Kids Cybertruck Tee',
+    description: 'Product Title',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   title: string;
 
+  @ApiProperty({
+    example: 25,
+    description: 'Product Price',
+    default: 0,
+  })
   @Column('float', {
     default: 0,
   })
   price: number;
 
+  @ApiProperty({
+    example:
+      'The Kids Cybertruck Tee features the iconic Cybertruck graffiti wordmark and is made from 100% Peruvian cotton for maximum comfort.',
+    description: 'Product Description',
+  })
   @Column('text', {
     nullable: true,
   })
   description: string;
 
+  @ApiProperty({
+    example: 'kids_cybertruck_tee',
+    description: 'Product Slug',
+    uniqueItems: true,
+  })
   @Column('text', {
     unique: true,
   })
   slug: string;
 
+  @ApiProperty({
+    example: 10,
+    description: 'Product Stock',
+    default: 0,
+  })
   @Column('int', {
     default: 0,
   })
   stock: number;
 
+  @ApiProperty({
+    example: ['XS', 'S', 'M'],
+    description: 'Product Sizes',
+  })
   @Column('text', {
     array: true,
   })
   sizes: string[];
 
+  @ApiProperty({
+    example: 'kid',
+    description: 'Product Gender',
+  })
   @Column('text')
   gender: string;
 
+  @ApiProperty({
+    example: ['shirt'],
+    description: 'Product Tags',
+    default: [],
+  })
   @Column('text', {
     array: true,
     default: [],
   })
   tags: string[];
 
+  @ApiProperty()
   @OneToMany(() => ProductImage, (productImage) => productImage.product, {
     cascade: true,
     eager: true,
